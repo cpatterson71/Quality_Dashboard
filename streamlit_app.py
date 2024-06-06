1e364d4c19282b738959948676344d8185e7cf87
cpatterson71 <carl.pattersonsd@gmail.com>
2024-06-06 16:19:20 -0700

    streamlit page added

---
+++ streamlit_app.py
@@ -0,0 +1,21 @@
+#streamlit-app.py
+import altair as alt
+import streamlit as st
+import Quality_Data_Clean_Up 
+from st_aggrid import Agrid
+
+st.set_page_config(layout='wide')
+st.title('Quality Managment System Dashboard')
+
+#set up tabs
+tab1, tab2, tab3 = st.tabs(['Change Controls', 'Deivations and OOS', 'CAPAs and Complaints'])
+
+with tab1:
+    x = temp['Date Opened']
+    y = temp['Days Open']
+    st.line_chart(x, y)
+
+with tab3:
+    buffer, col1, col2 = Agrid(
+
+
