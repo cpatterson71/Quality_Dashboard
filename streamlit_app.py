4d88538bdbdd52f4e1886c71f8cccd93a0d56cf3
cpatterson71 <carl.pattersonsd@gmail.com>
2024-06-18 14:58:56 -0700

    Update app

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,25 +11,32 @@ st.title('Quality Managment System Dashboard')
 #set up tabs
 tab1, tab2, tab3 = st.tabs(['Change Controls', 'Deivations and OOS', 'CAPAs and Complaints'])
 
+#load the data
+file = 'temp.csv'
+data = pd.read_csv(file)
+first = pd.DataFrame(data)
+
+#create x axis and strp down to date in Date Opened colum
+x = first['Date Opened'].astype(str)
+y = []
+for x in xs:
+    y.append(x[:-6])
+
+first['Date Opened'] = y
+
 with tab1:
     col1, col2 = st.columns([20, 20]) 
     with col1:
-        #load the data
-        file = 'temp.csv'
-        data = pd.read_csv(file)
-        first = pd.DataFrame(data)
-
-    #   #create x axis from 'Date Opened' to year
-        first['Date Opened'] = parse(first['Date Opened'])
-        first['Date Opened'] = first['Date Opened'].datetime.strftime('%YYYY')
+        
         x_axis = first['Date Opened']
 
-        #create y axis using count of change control number
-        y_axis = first['Change Control'].count()
+    #create y axis using count of change control number
+    y_axis = first['Change Control'].count()
 
-        #create bar chart using x_axis and y_axis
-        fig = px.bar(first, x=x_axis, y=y_axis)
-        fig.show()
+    #create bar chart using x_axis and y_axis
+    fig = px.bar(first, x=x_axis, y=y_axis)
+
+    fig.show()
 
         
     #     #create line chart
