6c54afc4a829a09784f8f633070bc20c12ee76c7
cpatterson71 <carl.pattersonsd@gmail.com>
2024-06-18 14:26:25 -0700

    Update to app

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ with tab1:
         first = pd.DataFrame(data)
 
     #   #create x axis from 'Date Opened' to year
-        first['Date Opened'] = datetime.strptime(first['Date Opened'], '%d/%m/%y').strftime('%Y')
+        first['Date Opened'] = datetime.strptime(first['Date Opened'], '%d/%m/%y').strftime('%YYYY')
         x_axis = first['Date Opened']
 
         #create y axis using count of change control number
