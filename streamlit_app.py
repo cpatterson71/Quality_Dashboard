4cbb899f26ec891d7d07c7230f64e3bdbaa48905
cpatterson71 <carl.pattersonsd@gmail.com>
2024-08-26 12:38:29 -0700

    Update

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,12 +60,18 @@ with tab1:
         #need to get number of change controls per month then year
         #use a chart similar to what was done above. 
     with tab2:
-         col4, col5 = st.columns([120, 120])
+         col4, col5, col8 = st.columns([120, 120, 120])
          with col4:
             AgGrid(first2, height=300)
 
          with col5:
              AgGrid(first3, height=300)
+
+         with col8:
+            third = pd.read_csv('Open_Dev.csv')
+            xp_axis = third['Year']
+            yp_axis = third['Count']
+            fig = px.bar(third,x=xp_axis, y=yp_axis ) 
 
     with tab3:
         col6, col7 = st.columns([100, 100])
